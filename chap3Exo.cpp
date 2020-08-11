245d84f4024f3590e6dbfe00df2ace6841fa096d
nouh ben kadi <benkadi.nouh@icloud.com>
2020-08-11 15:35:34 +0100

    training

--- chap3Exo.cpp
+++ chap3Exo.cpp
@@ -27,6 +27,41 @@ int compareStudentsByID(const void *studentA, const void *studentB)
 
     return (int)(_studentA->ID - _studentB->ID);
 }
+int compareINT(const void *number1, const void *number2)
+{
+    int *n1 = (int *)number1;
+    int *n2 = (int *)number2;
+    return *n1 - *n2;
+}
+double median(int *array, int size)
+{
+    int mid = size / 2;
+    int rest = size % 2;
+    qsort(array, size, sizeof(int), compareINT);
+    for (int student = 0; student < size; student++)
+    {
+        cout << array[student] << "\t";
+    }
+
+    if (rest == 0)
+    {
+        cout << "items = { " << array[mid - 1] << "|" << array[mid] << " sum= " << (array[mid - 1] + array[mid]) << " }\n";
+        return (array[mid - 1] + array[mid]) / 2;
+    }
+    cout << "items: " << array[mid - 1] << "\n";
+    return array[mid];
+}
+bool isSorted(int *array, int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        if (array[i] > array[i + 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
 int main()
 {
     const int ARRAY_SIZE = 10;
@@ -48,9 +83,22 @@ int main()
     {
         studentToString(studentsArray[student]);
     }
+    int gradesArray[ARRAY_SIZE] = {
+        14, 13, 4, 5, 7, 8, 8, 11, 11, 12}; // 4 5 7 8 8 11 11 12 13 14 ==> 11 +
+    qsort(gradesArray, ARRAY_SIZE, sizeof(int), compareINT);
+    char *test;
+    if ((isSorted(gradesArray, ARRAY_SIZE) == 1))
     {
-        /* code */
+        test = "True";
     }
+    else
+    {
+        test = "False";
+    }
+
+    cout << "is sorted: " << test << "\n";
+    double med = median(gradesArray, ARRAY_SIZE);
+    cout << "\nMedian grade: \t" << med << "\n";
 
     return 0;
 }
